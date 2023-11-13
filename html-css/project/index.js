5946345cbc6d61bb703b04bb38ebc8e7400f38b8
sivamudusu <sivamudusu@gmail.com>
2023-11-13 12:42:07 +0530

    java Script

--- html-css/project/index.js
+++ html-css/project/index.js
@@ -1,1 +1,77 @@
+class Thermostat {
+  constructor(temp) {
+    this._temp = temp;
+  }
+  get temp() {
+    return (5 / 9) * (this._temp - 32);
+  }
+  set temp(c) {
+    this._temp = (c * 9.0) / 5 + 32;
+  }
+}
 
+const thermos = new Thermostat(76);
+console.log(thermos.temp);
+thermos.temp = 26;
+console.log(thermos.temp);
+
+// let a = [
+//   [4, 5, 1, 3],
+//   [13, 27, 18, 26],
+//   [32, 35, 37, 39],
+//   [1000, 1001, 857, 1],
+// ];
+// a.sort();
+// console.log(a);
+// let string = "";
+// string += "hii";
+// string += "hello";
+// console.log(string);
+
+// let str = "hello world iam shiva the robot";
+// let arr = str.split(" ");
+// let max = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   max = Math.max(max, arr[i].length);
+// }
+// console.log(arr);
+// console.log(max);
+// let string = "hello iam shiva the robot";
+// let ans = titleCase(string);
+// console.log(ans);
+
+// function titleCase(str) {
+//   let ar = str.split(" ");
+//   for (let i = 0; i < ar.length; i++) {
+//     ar[i] = normalize(ar[i]);
+//   }
+//   let ans = ar.join(" ");
+//   return ans;
+// }
+// function normalize(str) {
+//   let a = str.substring(0, 1).toUpperCase();
+//   let b = str.substring(1, str.length).toLowerCase();
+
+//   let ab = a + b;
+//   return ab;
+// }
+// let arr = [5, 3, 20, 3];
+// console.log(arr);
+// arr.sort((a, b) => {
+//   return a - b;
+// });
+
+// console.log(arr);
+
+// let a1 = [4, 5];
+// let s2 = [1, 2, 3];
+
+// a1.splice(1, 0, ...s2);
+// console.log(a1);
+let arr = ["Alien", "line"];
+let str = arr[0].toLowerCase();
+let check = arr[1].toLowerCase();
+let ans = str.includes(check);
+console.log(ans);
+console.log(str);
+console.log(check);
---
+++ javascript/free-code-camp.md
@@ -0,0 +1,6 @@
+![Basic JavaScript](<Screenshot 2023-11-13 123038.png>);
+![ES6](<Screenshot 2023-11-13 123053.png>)
+![Regular Expressions](<Screenshot 2023-11-13 123108.png>)
+![Debugging](<Screenshot 2023-11-13 123123.png>)
+![Basic Algo Scripting](<Screenshot 2023-11-13 123140.png>)
+![OOPS](<Screenshot 2023-11-13 123155.png>)
